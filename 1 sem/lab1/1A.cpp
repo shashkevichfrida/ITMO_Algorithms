b37c1371d20e01a6ab605a0f1cc00ce8b05eb7fd
shashkevichfrida <89972439+shashkevichfrida@users.noreply.github.com>
2022-07-03 16:11:14 +0300

    Add files via upload

---
+++ 1 sem/lab1/1A.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+
+using namespace std;
+
+int main ()
+{
+  int a, b;
+  ifstream fin ("aplusb.in");
+  ofstream fout ("aplusb.out");
+  fin >> a >> b;
+  fout << a + b << endl;
+  fin.close ();
+  fout.close ();
+  return 0;
+}
